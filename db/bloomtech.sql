c81f46651f1217a18227be8898b9d6c6aa0aae07
Maksu258 <116091064+Maksu258@users.noreply.github.com>
2023-01-24 17:26:40 +0100

    Add files via upload

--- db/bloomtech.sql
+++ db/bloomtech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 20 jan. 2023 à 11:25
+-- Généré le : mar. 24 jan. 2023 à 17:25
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 7.4.33
 
@@ -42,7 +42,7 @@ CREATE TABLE `faq` (
 CREATE TABLE `glassesdata` (
   `iddata` int(11) NOT NULL,
   `idglasses` varchar(11) NOT NULL,
-  `date` date NOT NULL,
+  `datedata` date NOT NULL,
   `screentime` int(11) DEFAULT NULL,
   `CO2` int(11) DEFAULT NULL,
   `heartbeat` int(11) DEFAULT NULL,
@@ -53,8 +53,15 @@ CREATE TABLE `glassesdata` (
 -- Déchargement des données de la table `glassesdata`
 --
 
-INSERT INTO `glassesdata` (`iddata`, `idglasses`, `date`, `screentime`, `CO2`, `heartbeat`, `noise`) VALUES
-(1, 'test', '2023-01-20', 7, 5, 10, 23);
+INSERT INTO `glassesdata` (`iddata`, `idglasses`, `datedata`, `screentime`, `CO2`, `heartbeat`, `noise`) VALUES
+(1, 'test', '2023-01-12', 7, 5, 10, 23),
+(2, 'test', '2023-01-13', 8, 2, 3, 7),
+(3, 'test', '2023-01-14', 5, 6, 4, 10),
+(4, 'test', '2023-01-15', 5, 6, 4, 10),
+(5, 'test', '2023-01-16', 5, 6, 4, 10),
+(6, 'test', '2023-01-17', 8, 2, 3, 7),
+(7, 'test', '2023-01-18', 7, 5, 10, 23),
+(8, 'test', '2023-01-19', 7, 5, 10, 23);
 
 -- --------------------------------------------------------
 
@@ -102,7 +109,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `firstName`, `lastName`, `dateOfBirth`, `phone`, `address`, `email`, `admin`, `password`, `idglasses`, `Facebook`, `Instagram`, `Twitter`) VALUES
 (3, 'Kevin', 'Rizk', '2000-12-29', NULL, NULL, 'kevin.rizk14@gmail.com', 0, '202cb962ac59075b964b07152d234b70', NULL, NULL, NULL, NULL),
 (4, 'george ', 'george ', '1999-12-12', NULL, NULL, '13@gmail.com', 0, '202cb962ac59075b964b07152d234b70', NULL, NULL, NULL, NULL),
-(5, 'test', 'test', '2023-01-01', NULL, NULL, 'test@test.com', 0, '098f6bcd4621d373cade4e832627b4f6', 'test', NULL, NULL, NULL);
+(5, 'test', 'test', '2023-01-01', NULL, NULL, 'test@test.com', 0, '098f6bcd4621d373cade4e832627b4f6', 'test', NULL, NULL, NULL),
+(6, 'test2', 'test2', '2023-01-01', NULL, NULL, 'test2@test.com', 0, '098f6bcd4621d373cade4e832627b4f6', 'test2', NULL, NULL, NULL),
+(10, 'test3', 'test3', '2023-01-01', NULL, NULL, 'test3@test.com', 0, '098f6bcd4621d373cade4e832627b4f6', NULL, NULL, NULL, NULL);
 
 --
 -- Index pour les tables déchargées
@@ -119,7 +128,7 @@ ALTER TABLE `faq`
 --
 ALTER TABLE `glassesdata`
   ADD PRIMARY KEY (`iddata`),
-  ADD KEY `glassesdata_ibfk_1` (`idglasses`);
+  ADD KEY `idglasses` (`idglasses`);
 
 --
 -- Index pour la table `opticians`
@@ -150,7 +159,7 @@ ALTER TABLE `faq`
 -- AUTO_INCREMENT pour la table `glassesdata`
 --
 ALTER TABLE `glassesdata`
-  MODIFY `iddata` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `iddata` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `opticians`
@@ -162,7 +171,7 @@ ALTER TABLE `opticians`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Contraintes pour les tables déchargées
@@ -172,7 +181,7 @@ ALTER TABLE `users`
 -- Contraintes pour la table `glassesdata`
 --
 ALTER TABLE `glassesdata`
-  ADD CONSTRAINT `glassesdata_ibfk_1` FOREIGN KEY (`idglasses`) REFERENCES `users` (`idglasses`) ON UPDATE NO ACTION;
+  ADD CONSTRAINT `glassesdata_ibfk_1` FOREIGN KEY (`idglasses`) REFERENCES `users` (`idglasses`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
