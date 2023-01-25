3d65c178401ac8f2f49f99c6e0bfd4b259dbf1f5
Maksu258 <maxence@greus.fr>
2023-01-25 22:37:59 +0100

    final modif

--- location.php
+++ location.php
@@ -1,5 +1,6 @@
 <?php
     include 'includes/header.inc.php';
+    include 'includes/connexion.inc.php';
 ?>
 
 <main>
@@ -11,48 +12,56 @@
         <div class="optician-addresses">
         <div class="boxes">
             <ul class="list-of-addresses">
+            <li class="li-2">
+                <h4 style="margin-top:0; text-align: center;">LISTE DES OPTICIENS<hr>
+            </li>
 
-                <li class="li-2">
-                <h4 style="margin-top:0;">OPTICIAN 1</h4><br>
-                <span>
-                    138 Av. des Champs-Élysées · 00 00 00 00 00<br>
-                    Open ⋅ Closes 9PM<br>
-                    In-store shopping·<br>
-                    In-store pick-up (Example)<br>
-                    </span><hr>
-                </li>
+        <?php
+        $conn=OpenCon();
+        $query='SELECT * FROM opticians';
+        $req = mysqli_query($conn, $query);
+        $data=mysqli_fetch_assoc($req);
+        foreach ($req as $data){
+            $name=$data['opticianname'];
+            $adress=$data['adressoptic'];
+            $time=$data['openinghours'];
+            $phone=$data['phoneoptic'];
+            $stock=$data['stock'];
+        ?>
 
-                <li class="li-2">
-                <h4>OPTICIAN 2</h4><br>
-                <span>
-                    138 Av. des Champs-Élysées · 00 00 00 00 00<br>
-                    Open ⋅ Closes 9PM<br>
-                    In-store shopping·<br>
-                    In-store pick-up (Example)<br>
-                    </span><hr>
-                </li>
-
-                <li class="li-2">
-                <h4>OPTICIAN 3</h4><br>
-                <span>
-                    138 Av. des Champs-Élysées · 00 00 00 00 00 <br>
-                    Open ⋅ Closes 9PM<br>
-                    In-store shopping·<br>
-                    In-store pick-up (Example)
-                    </span>
-                </li>
+            <li class="li-2">
+                <h4 style="margin-top:0;"><?php echo "$name"?></h4><br>
+                <span>Adresse : <?php echo "$adress"?> </br>
+                Numéro de téléphone : <?php echo "$phone"?></br>
+                Horaires d'ouverture : <?php echo "$time"?> </br>
+                En stock :
+                <?php if ("$stock"=="Non"){
+                    ?> NON <?php
+                } else {
+                    ?> OUI <?php
+                }
+                ?>
+                </span><hr>
+            </li>
+<?php
+}
+?>
 
             </ul>
 
                 <div class="box-info">
                     <div class="title">
-                        <h2>Besoin plus d'information?</h2>
+                        <h2>Besoin de plus d'information?</h2>
                     </div>
                     <div class="information">
                         <span>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        Voici la liste des opticiens partenaires. </br>
+                        Vous y trouverez leurs coordonnées et la disponibilité de nos produits. </br>
+                        Avant tout achat, veuillez consulter un ophtalmologue afin de pouvoir acheter des lunettes adaptées à votre vue.</br>
+                        Pour plus d'information sur nos lunettes Binoclear ou pour nous contacter, veuillez cliquer sur les boutons ci-dessous.
                         </span><br>
                     <div class="btn-info">
+                        <a class="harik-style" href="FAQ.php">FAQ</a>
                         <a class="harik-style" href="nous-contacter.php">Nous contacter</a>
                     </div>
 
